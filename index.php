d191056cd1634ad08c77150593ecc5c6006afb8b
JosueBunker <josue@bunkeraccess.com>
2018-11-12 23:12:06 -0600

    .

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <title>Fototeca - Home</title>
 
   <!-- Icons -->  
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+  <link href="//fototeca.bunker.media/wp-content/themes/fototeca/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
   <!-- css -->  
   <link href="//fototeca.bunker.media/wp-content/themes/fototeca/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
   <link href="//fototeca.bunker.media/wp-content/themes/fototeca/css/style.css" rel="stylesheet" type="text/css" media="all">
